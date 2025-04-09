97a6612864dd7b6d35d08de58f3f91b4b2eecb5a
jcostaaaaaa <jimmynobile0@gmail.com>
2025-04-09 15:12:43 +0100

    Convert home page to Client Component to fix event handler error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Header } from "@/components/Header";
 import Link from "next/link";
 import { FaFileUpload, FaChartBar, FaSearch, FaLightbulb, FaServer, FaNetworkWired, FaDatabase, FaComments, FaChartPie } from "react-icons/fa";
